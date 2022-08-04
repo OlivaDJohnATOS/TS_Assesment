43eefead9b5734eb54f54e2cb95dc7e44adee5b1
Jonathan Javier Oliva Dominguez (ext) <jonathan.oliva.external@atos.net>
2022-08-04 14:24:19 -0700

    Assesment 2 Practice

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.ts')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ]
+}
